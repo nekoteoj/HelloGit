6c60ae3a76582f6c321651d5b10d7827b69597fc
Pisit Wajanasara <compisit1234@gmail.com>
2017-08-26 19:56:57 +0700

    add beta comment

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("Edited on other computer");
+		// This is beta version
 	}
 
 }
